03f1ec81db6dd9d81b83341ce8584166d9fff79a
Julia Tsukerman <julia.tsuk@gmail.com>
2021-11-07 15:40:14 +0300

    TextFields and Keyboard Done Button added.

--- HW 2/SettingsViewController.swift
+++ HW 2/SettingsViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-    
+   
+    //MARK: - IBOutlets
     @IBOutlet weak var colorView: UIView!
 
     @IBOutlet weak var redLabel: UILabel!
@@ -20,9 +21,15 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
+    @IBOutlet var redSliderTF: UITextField!
+    @IBOutlet var greenSliderTF: UITextField!
+    @IBOutlet var blueSliderTF: UITextField!
+    
+    //MARK: - Public properties
     var delegate: SettingsViewControllerDelegate?
     var sentColor: UIColor!
     
+    //MARK: - Navigation
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,12 +38,25 @@ class SettingsViewController: UIViewController {
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         
+        redSliderTF.delegate = self
+        greenSliderTF.delegate = self
+        blueSliderTF.delegate = self
+        
         colorView.backgroundColor = sentColor
+        
         setSlidersValue()
         setValue(for: redLabel, greenLabel, blueLabel)
+        setValueForTF(for: redSliderTF,greenSliderTF, blueSliderTF)
+        
+        addDoneButtonTo(textField: redSliderTF)
+        addDoneButtonTo(textField: greenSliderTF)
+        addDoneButtonTo(textField: blueSliderTF)
+                
     }
     
+    //MARK: - IBAction functions
     @IBAction func doneButtonPress() {
+        
         let color = colorView.backgroundColor
         delegate!.setNewColor(for: color ?? .white)
         dismiss(animated:true)
@@ -47,15 +67,18 @@ class SettingsViewController: UIViewController {
         switch sender {
         case redSlider:
             redLabel.text = string(from: redSlider)
+            redSliderTF.text = string(from: redSlider)
         case greenSlider:
             greenLabel.text = string(from: greenSlider)
+            greenSliderTF.text = string(from: greenSlider)
         default:
             blueLabel.text = string(from: blueSlider)
+            blueSliderTF.text = string(from: blueSlider)
         }
         setColor()
     }
     
-
+    //MARK: - Private functions
   private func setColor() {
         colorView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
@@ -78,10 +101,26 @@ class SettingsViewController: UIViewController {
         }
     }
     
+    private func setValueForTF(for textfields: UITextField...){
+        textfields.forEach { textfield in
+        switch textfield {
+        case redSliderTF:
+            redSliderTF.text = string(from: redSlider)
+       case greenSliderTF:
+            greenSliderTF.text = string(from: greenSlider)
+        default:
+            blueSliderTF.text = string(from: blueSlider)
+        }
+      }
+    }
+   
     private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
     }
 }
+
+//MARK: - Extensions
+
 extension SettingsViewController {
     private func setSlidersValue()  {
         var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alfa: CGFloat){
@@ -100,4 +139,57 @@ extension SettingsViewController {
         let blueSliderValue = rgba.blue
         blueSlider.value = Float(blueSliderValue)
         }
+    
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    private func addDoneButtonTo (textField:UITextField){
+                let bar = UIToolbar()
+                let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+                let done = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneButtonTapped))
+                bar.items = [flexibleSpace,done]
+                bar.sizeToFit()
+                textField.inputAccessoryView = bar
+            }
+
+             @objc private func doneButtonTapped() {
+                 view.endEditing(true)
+            }
+        
+    }
+
+
+//MARK: - UITextFieldDelegate
+extension SettingsViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let textValue = textField.text else {return}
+        if let inputTextValue = Float(textValue){
+            switch textField.tag {
+            case 0: redSlider.value = inputTextValue
+            case 1: greenSlider.value = inputTextValue
+            case 2: blueSlider.value = inputTextValue
+            default: break
+            }
+            setColor()
+            setValue(for: redLabel, greenLabel, blueLabel)
+            
+        } else {
+            showAlert(title: "Wrong format", message: "Please enter correct data")
+        }
+    }
 }
--- HW 2/StartViewController.swift
+++ HW 2/StartViewController.swift
@@ -13,30 +13,11 @@ protocol SettingsViewControllerDelegate {
 }
 class StartViewController: UIViewController {
 
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let settingsVC = segue.destination as? SettingsViewController else {return}
         settingsVC.sentColor = view.backgroundColor
         settingsVC.delegate = self
-      
-        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 //MARK: - SettingsViewControllerDelegate
 extension StartViewController:SettingsViewControllerDelegate {
